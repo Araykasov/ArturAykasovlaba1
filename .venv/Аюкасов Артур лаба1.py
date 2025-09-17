2c3c33c89f4a00c07dde733d691d298a889e2cb5
Araykasov <arayukasov@gmail.com>
2025-09-17 13:39:25 +0500

    Update Аюкасов Артур лаба1.py
    Добавляю новую строчку песни через гит

--- .venv/Аюкасов Артур лаба1.py
+++ .venv/Аюкасов Артур лаба1.py
@@ -8,9 +8,11 @@ def proverca_3():
     print('17.09.2025')
 def proverca_4():
     print('Не найти сокровища Энии - времени пыль смыло волной ')
+def proverca_5():
+    print('И летят мои сноведения')
 if __name__ == '__main__':
     main()
     proverca_2()
     proverca_3()
     proverca_4()
-    
+    proverca_5()

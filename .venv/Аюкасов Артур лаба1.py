0bf85e46238e91ab1fa7ee5417c14161e8c91e73
Ayukasov Artur <arayukasov@gmail.com>
2025-09-17 12:33:14 +0500

    Добавил исполнение метода proverca_4

---
+++ .venv/Аюкасов Артур лаба1.py
@@ -0,0 +1,14 @@
+def main():
+    print('Аюкасов Артур')
+def proverca_1():
+    main()
+def proverca_2():
+    print('ИВТ-ИВС-202Б, 1 подгрупа')
+def proverca_3():
+    print('17.09.2025')
+def proverca_4():
+    print('Не найти сокровища Энии - времени пыль смыло волной ')
+if __name__ == '__main__':
+    main()
+    proverca_4()
+    
